66dfdb442962d7fa8196018b342e4a079293d46e
Max Shipitsyn <m.o.shipitsyn@gmail.com>
2022-03-31 23:55:37 +0600

    12.03 ready

--- 12.03-kube-install-1/terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "external_ip_control_plane_01" {
-  value = yandex_compute_instance.cp-01.network_interface.0.nat_ip_address
-}
-
-output "external_ip_node_01" {
-  value = yandex_compute_instance.node-01.network_interface.0.nat_ip_address
-}
