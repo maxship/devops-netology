94ac16c9e0cd517afec68bd0d83d529b3d3ef4ef
Max Shipitsyn <m.o.shipitsyn@gmail.com>
2022-04-02 00:51:35 +0600

    12.05 start

---
+++ 12.05-kube-cni/terraform/output.tf
@@ -0,0 +1,7 @@
+output "external_ip_control_plane" {
+  value = yandex_compute_instance.cp-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_work_nodes" {
+  value = yandex_compute_instance.node[*].network_interface.0.nat_ip_address
+}
